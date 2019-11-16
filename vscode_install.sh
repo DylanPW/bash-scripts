c5cb876cc927b222d05beea27d73f58ca89886b3
DylanPW <d.prykewatanabe@gmail.com>
2019-11-16 19:27:26 +1000

    initial commit

---
+++ vscode_install.sh
@@ -0,0 +1,6 @@
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install software-properties-common apt-transport-https wget -y
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+sudo apt update -y
+sudo apt install code -y
